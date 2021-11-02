25d7caf74d38193859a38b8c96e1a91ba802d93a
Samuele <miotto.samuele@gmail.com>
2021-11-02 16:54:12 +0100

    pseudocode

---
+++ script.js
@@ -0,0 +1,14 @@
+/*
+
+1. far selezionare la difficoltà -> promt/pulsanti
+    1.1 a seconda della difficoltà generare la griglia (facile 10x10, medio 9x9, difficile 7x7) -> if con tre funzioni di generazione degli elementi
+2. selezionata la difficoltà generare la griglia
+    2.1 selezionare il contenitore della griglia
+    2.2 generare il singolo div
+    2.3 applicare le classi di stilizzazione e inserire il numero relativo al quadrato a seconda della difficoltà (1-100 / 1-81 / 1-49)
+    2.4 generare il numero corretto di quadrati all'interno della griglia a seconda della difficoltà
+    (tutto questo sta in una funzione - la posso richiamare al click o inserendo la difficoltà nel prompt)
+3. al clic del giocatore cambiare il colore del singolo quadratino -> aggiungere classe active che cambia lo sfondo con addEventListener
+
+
+*/
